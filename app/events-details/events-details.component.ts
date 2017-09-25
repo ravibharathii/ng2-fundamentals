6318226e2bfa8ee6a117be7c7bbe5c999774841a
raviramamoorthy <ravibharathii@gmail.com>
2017-09-25 23:14:27 +0530

    git ignore

--- app/events-details/events-details.component.ts
+++ app/events-details/events-details.component.ts
@@ -1,3 +1,13 @@
 /**
  * Created by raviramamoorthy on 9/25/17.
  */
+import {Component} from "@angular/core";
+
+@Component({
+    selector:'event-detail',
+    templateUrl:'app/events-details/event-details.component.html'
+
+})
+export class EventsDetailsComponent{
+
+}
